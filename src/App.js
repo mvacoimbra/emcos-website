c19f4249b56cda3cfac5ba1787f4bd62b26ae600
mvacoimbra <mv27.marcosvinicius@gmail.com>
2023-08-24 20:54:40 -0300

    fixing scroll position on page select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 // pages
 import HomePage from './pages/Home';
 import PortfolioPage from './pages/Portfolio';
 import ContactPage from './pages/Contact';
 import BudgetPage from './pages/Budget';
-import TermsPage from './pages/Terms'
+import TermsPage from './pages/Terms';
 // css
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'auto',
+    });
+  }, [location]);
+
   return (
     <div>
       <Routes>
-        <Route path='/' Component={HomePage}/>
-        <Route path='/portfolio' Component={PortfolioPage}/>
-        <Route path='/contact' Component={ContactPage}/>
-        <Route path='/budget' Component={BudgetPage}/>
-        <Route path='/terms' Component={TermsPage}/>
+        <Route path="/" Component={HomePage} />
+        <Route path="/portfolio" Component={PortfolioPage} />
+        <Route path="/contact" Component={ContactPage} />
+        <Route path="/budget" Component={BudgetPage} />
+        <Route path="/terms" Component={TermsPage} />
       </Routes>
     </div>
   );
